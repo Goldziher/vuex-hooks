f1c3eab01c25005e2b51baf293c3f6271f147a2a
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2020-05-03 19:32:10 +0200

    added build config and prettier

---
+++ rollup.config.js
@@ -0,0 +1,39 @@
+import typescript from 'rollup-plugin-typescript2'
+
+const input = 'src/index.ts'
+const output = (type) => ({
+	output: {
+		exports: 'named',
+		file: `dist/${type}/index.js`,
+		format: type,
+		name: 'vuex-hooks',
+		sourcemap: true,
+	},
+})
+const plugins = (useDeclaration) => ({
+	plugins: typescript({
+		useTsconfigDeclarationDir: true,
+		tsconfigOverride: {
+			compilerOptions: {
+				target: 'ESNEXT',
+				declaration: useDeclaration,
+			},
+		},
+	}),
+})
+const external = ['vue', 'vuex', '@vue/composition-api']
+
+export default [
+	{
+		input,
+		external,
+		...output('esm'),
+		...plugins(true),
+	},
+	{
+		input,
+		external,
+		...output('cjs'),
+		...plugins(false),
+	},
+]
